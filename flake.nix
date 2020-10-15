6718e4f851347ce34d8fbabf516c6a0da85966f0
Shay Bergmann <sbergmann@kreisys.co>
2020-10-15 05:57:03 -0400

    Clean up

--- flake.nix
+++ flake.nix
@@ -4,15 +4,11 @@
   inputs.utils.url = "github:numtide/flake-utils";
 
   outputs = { self, utils, nixpkgs }:
-    utils.lib.eachDefaultSystem (system: rec {
+    utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system: rec {
       overlay = final: prev: {
         inherit (final.callPackages ./default.nix {
           inherit (final.llvmPackages_10) stdenv clang llvm;
         })
-          crystal_0_31
-          crystal_0_32
-          crystal_0_33
-          crystal_0_34
           crystal
           crystal2nix;
       };
@@ -23,19 +19,11 @@
       };
 
       packages = {
-        inherit (legacyPackages) crystal;
+        inherit (legacyPackages) crystal crystal2nix;
       };
 
       defaultPackage = legacyPackages.crystal;
 
       hydraJobs = packages;
     });
-
 }
-
-# {
-
-#   packages.x86_64-darwin.crystal = nixpkgs.legacyPackages.x86_64-linux.hello;
-#   packages.x86_64-darwin = {
-
-#   defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
