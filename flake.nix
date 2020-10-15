cdec6e96b790fabc9d81ac7da433895fa8acb4a7
Shay Bergmann <sbergmann@kreisys.co>
2020-10-15 01:45:24 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, utils, nixpkgs }:
+    utils.lib.eachDefaultSystem (system: rec {
+      overlay = final: prev: {
+        inherit (final.callPackages ./default.nix {
+          inherit (final.llvmPackages_10) stdenv clang llvm;
+        })
+          crystal_0_31
+          crystal_0_32
+          crystal_0_33
+          crystal_0_34
+          crystal
+          crystal2nix;
+      };
+
+      legacyPackages = import nixpkgs {
+        inherit system;
+        overlays = [ overlay ];
+      };
+
+      packages = {
+        inherit (legacyPackages) crystal;
+      };
+
+      defaultPackage = legacyPackages.crystal;
+    });
+
+}
+
+# {
+
+#   packages.x86_64-darwin.crystal = nixpkgs.legacyPackages.x86_64-linux.hello;
+#   packages.x86_64-darwin = {
+
+#   defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
