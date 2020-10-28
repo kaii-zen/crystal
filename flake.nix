e02fc54d7a3a46e185b181f3da77eff4053ad4a2
Shay Bergmann <sbergmann@kreisys.co>
2020-10-28 07:59:29 -0400

    Refactor using flake-utils.simpleFlake

--- flake.nix
+++ flake.nix
@@ -1,39 +1,25 @@
 {
-  description = "A very basic flake";
+  description = "The Crystal Programming Language";
 
   inputs.utils.url = "github:numtide/flake-utils";
 
   outputs = { self, utils, nixpkgs }:
-    utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system: rec {
+    let
+      name = "crystal-flake";
+      systems = [ "x86_64-darwin" "x86_64-linux" ];
       overlay = final: prev: {
-        inherit (final.callPackages ./default.nix {
+        crystal-flake = final.callPackages ./default.nix {
           inherit (final.llvmPackages_10) stdenv clang llvm;
-        })
-          crystal_0_31
-          crystal_0_33
-          crystal_0_34
-          crystal_0_35
-          crystal
-          crystal2nix;
+        };
       };
 
-      legacyPackages = import nixpkgs {
-        inherit system;
-        overlays = [ overlay ];
+      simpleFlake = utils.lib.simpleFlake {
+        inherit name systems overlay self nixpkgs;
       };
+    in
+    simpleFlake // {
+      inherit overlay;
 
-      packages = {
-        inherit (legacyPackages)
-          crystal_0_31
-          crystal_0_33
-          crystal_0_34
-          crystal_0_35
-          crystal
-          crystal2nix;
-      };
-
-      defaultPackage = legacyPackages.crystal;
-
-      hydraJobs = packages;
-    });
+      hydraJobs = self.packages;
+    };
 }
